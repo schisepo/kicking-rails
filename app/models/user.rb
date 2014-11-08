2785399bb412ee52e91f0c14293d5b0d60e3f59a
schisepo <schisepo@gmail.com>
2014-11-08 19:56:15 +0200

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
 	#attr_accessible :name,:email,:password,:password_confirmation
 	has_secure_password
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	before_save { |user| user.email = email.downcase }
+	#before_save { |user| user.email = email.downcase }
+	before_save { self.email.downcase! }
 	validates(:name,presence:true,length:{maximum:50})
 	validates(:email,presence:true,format:{with: VALID_EMAIL_REGEX},uniqueness:{case_sensitive:false})
 	validates(:password,presence:true,length:{minimum:6})	
